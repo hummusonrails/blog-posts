795738b297202db9a62c0bc2e07020c1fc1b8e94
Ben Greenberg <hello@yalladevrel.com>
2024-05-20 08:43:38 -0400

    Remove node_modules directory from repository

--- node_modules/couchbase/dist/sdutils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class SdUtils {
-    private static _parsePath;
-    private static _insertByPath;
-    static insertByPath(root: any, path: string, value: any): any;
-    private static _getByPath;
-    static getByPath(value: any, path: string): any;
-    static convertMacroCasToCas(cas: string): string;
-}
--- node_modules/couchbase/dist/utilities_internal.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @internal
- */
-export declare function generateClientString(): string;
